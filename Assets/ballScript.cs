62266bdb37bd9f54031213f8c3f728989634c381
Jabrecia Washington <jlw4958@g.rit.edu>
2022-12-16 18:50:52 -0500

    ball movement done

--- Assets/ballScript.cs
+++ Assets/ballScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ballScript : MonoBehaviour
 {
+    public Rigidbody2D ballRigidBody;
+    public int velocityChange;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class ballScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ballRigidBody.velocity = Vector2.up * velocityChange;
+        }
     }
 }
